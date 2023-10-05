381abcfddfc130c5a3e54f8836debbc11366b26b
Jeankory V <124745650+jeankory500@users.noreply.github.com>
2023-10-04 21:13:06 -0400

    Update navbar.js

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useContext} from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const Navbar = () => {
+	const { store, actions } = useContext(Context);
+
 	return (
 		<nav className="navbar navbar-light bg-light">
 			<div className="container">
 				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+					<span className="navbar-brand mb-0 h1">JWT Project</span>
 				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+				<div>
+					{ !store.token ? 
+						<Link to="/">
+							<button className="btn btn-primary">Login</button>
+						</Link>
+						
+						:
+
+						<button onClick={() => actions.logout()} className="btn btn-primary">Logout</button>
+
+					}
+
 				</div>
 			</div>
 		</nav>
 	);
 };
+
