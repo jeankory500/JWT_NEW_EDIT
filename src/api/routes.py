cd4c41181ab5f6c0d3c1db7f50dc8544fe72c982
Jeankory V <124745650+jeankory500@users.noreply.github.com>
2023-10-04 21:10:42 -0400

    Update routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,55 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad username or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+@api.route('/private', methods=['GET'])
+@jwt_required()
+def private_route():
+    
+    email = get_jwt_identity()
+    response = {
+        "message": f"Hello, {email}! This is a protected route."
     }
 
-    return jsonify(response_body), 200
+    return jsonify(response), 200
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+
+    # Check if the email already exists in the database
+    existing_user = User.query.filter_by(email=data['email']).first()
+    if existing_user:
+        return jsonify({"message": "Email already registered"}), 400
+
+    # Create a new user
+    new_user = User(
+        email=data['email'],
+        password=data['password'],
+        is_active=True,
+    )
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"message": "User registered successfully"}), 201
+
