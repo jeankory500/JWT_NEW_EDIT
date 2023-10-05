a372f0080c773545bf89443d284059492a6d73f3
Jeankory V <124745650+jeankory500@users.noreply.github.com>
2023-10-04 21:17:12 -0400

    Update flux.js

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,7 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			token: null,
 			message: null,
 			demo: [
 				{
@@ -16,37 +17,82 @@ const getState = ({ getStore, getActions, setStore }) => {
 			]
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			syncTokenFromSessionStore: () => {
+				const token = sessionStorage.getItem("token");
+				console.log("Application just loaded, syncing session storage token");
+				if (token && token !== "" && token !== undefined) setStore({ token: token });
+			},
+
+			signup: async (email, password) => {
+				const opts = {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({
+						email: email,
+						password: password,
+					}),
+				};
+				
+				try {
+					const resp = await fetch(`${[YOUR_BACKEND_URL]}/api/signup`, opts);
+					if (resp.status === 201) {
+						console.log("Signup successful!");
+					} else {
+						console.error("Signup failed");
+					}
+				} catch (error) {
+					console.error("Error during signup", error);
+				}
+			},  
+
+			logout: () => {
+				sessionStorage.removeItem("token");
+				console.log("Logging Out");
+				setStore({ token: null });
+			},
+
+			login: async (email, password) => {
+				const opts = {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({
+						"email": email,
+						"password": password,
+					}),
+				};
+
+				try {
+					const resp = await fetch("https://obscure-robot-x6gjwv9jxq926xrr-3001.app.github.dev/api/token", opts);
+					if(resp.status !== 200) {
+						alert("There was an error");
+						return false;
+					}
+					
+					const data = await resp.json();
+					sessionStorage.setItem("token", data.access_token);
+					setStore({ token: data.access_token });
+					return true;
+				} catch(error) {
+					console.error("Login error", error);
+				}
 			},
 
 			getMessage: async () => {
 				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
+					const resp = await fetch(`$https://obscure-robot-x6gjwv9jxq926xrr-3001.app.github.dev/api/hello`);
+					const data = await resp.json();
+					setStore({ message: data.message });
 					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
+				} catch(error) {
+					console.error("Error loading message from backend", error);
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+			
 		}
 	};
 };
